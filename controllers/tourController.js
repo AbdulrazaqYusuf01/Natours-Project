d0e84b6c6362c01143ec294cbd7feacce44dfa12
Abdulrazaq <abdulrazaqmyusuf@gmail.com>
2025-10-04 10:39:14 +0100

    update recent file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,28 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  if (Number(req.params.id) > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price)
+    return res.status(400).json({
+      status: 'Error',
+      message: 'Bad request',
+    });
+  next();
+};
+
 exports.getAllTours = (req, res) => {
+  console.log('This is from all the tours');
   res.status(200).send({
     status: 200,
     result: tours.length,
@@ -14,13 +35,8 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
+  console.log('This is from the only tour');
   const tour = tours.find((el) => el.id === Number(req.params.id));
-
-  if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
 
   res.status(200).send({
     status: 200,
@@ -50,12 +66,6 @@ exports.createNewTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
   res.status(200).json({
     status: 'Success',
     data: {
@@ -65,12 +75,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
   res.status(204).json({
     status: 'Success',
     data: null,
